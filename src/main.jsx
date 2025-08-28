077e4a2d57c0ddfda1073da6371514241eba92de
andreas893 <eaa24andrh@students.eaaa.dk>
2025-08-28 15:07:39 +0200

    push2

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename={import.meta.env.DEV ? "/" : "react-router-opg"}>
       <App />
     </BrowserRouter>
   </StrictMode>
